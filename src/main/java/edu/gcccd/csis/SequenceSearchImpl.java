d3c091d74322e329a9c0252a19dc70dace84386b
mpinko <35934292+mpinko@users.noreply.github.com>
2018-04-02 16:03:42 -0700

    Update SequenceSearchImpl.java

--- src/main/java/edu/gcccd/csis/SequenceSearchImpl.java
+++ src/main/java/edu/gcccd/csis/SequenceSearchImpl.java
@@ -2,27 +2,39 @@ package edu.gcccd.csis;
 
 public class SequenceSearchImpl extends SequenceSearch {
 
-    public SequenceSearchImpl(final String content, final String start, final String end) {
-        super(content, start, end);
-    }
+	public SequenceSearchImpl(final String content, final String start,
+			final String end) {
+		super(content, start, end);
+	}
 
-    @Override
-    public String[] getAllTaggedSequences() {
-        String[] sa = {};
-        for (int i = 0; i < content.length() - endTag.length() - startTag.length() + 1; i++) {
-            if (content.charAt(i) == startTag.charAt(0) && content.charAt(i + startTag.length() - 1) == startTag.charAt(startTag.length() - 1)) {
-                int startString = i + startTag.length();
-                for (int j = i + 1 + startTag.length(); j < content.length() - endTag.length() + 1; j++) {
-                    if (content.charAt(j) == endTag.charAt(0) && content.charAt(j + endTag.length() - 1) == endTag.charAt(endTag.length() - 1)) {
-                        int endString = j;
-                        sa = adds(sa, content.substring(startString, endString));
-                        break;
-                    }
-                }
-            }
-        }
-        return sa;
-    }
+	@Override
+	public String[] getAllTaggedSequences() {
+		String[] sa = {};
+		String tmpContent = content;
+		boolean found = false;
+		do {
+			found = false;
+			for (int i = 0; i < tmpContent.length() - endTag.length() - startTag.length() + 1; i++) 
+			{
+				if (tmpContent.charAt(i) == startTag.charAt(0) && tmpContent.charAt(i + startTag.length() - 1) == startTag.charAt(startTag.length() - 1)) 
+				{
+					int startString = i + startTag.length();
+					for (int j = i + 1 + startTag.length(); j < tmpContent.length() - endTag.length() + 1; j++) 
+					{
+						if (tmpContent.charAt(j) == endTag.charAt(0) && tmpContent.charAt(j + endTag.length() - 1) == endTag.charAt(endTag.length() - 1)) 
+						{
+							int endString = j;
+							sa = adds(sa, tmpContent.substring(startString, endString));
+							tmpContent = tmpContent.substring(endString+1, tmpContent.length());
+							found = true;
+							break;
+						}
+					}
+				}
+			}
+		} while (!found);
+		return sa;
+	}
 
     @Override
     public String getLongestTaggedSequence() {
@@ -42,12 +54,25 @@ public class SequenceSearchImpl extends SequenceSearch {
 
     @Override
     public String displayStringArray() {
-        return null;
+    	String[] sa = getAllTaggedSequences();
+    	String s = "";
+        for(int i = 0; i < sa.length; i++)
+        {
+        	s += " " + sa[i];
+        }
+        return s;
     }
 
     @Override
     public String toString() {
-        return null;
+    	String[] sa = getAllTaggedSequences();
+    	String s = "";
+    	for(int i = 0; i < sa.length; i++)
+    	{
+    		s+= " " + sa[i];
+    	}
+        return s;
     }
 
 }
+
